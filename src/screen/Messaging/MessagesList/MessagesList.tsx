ca32f5127752734be233cfb1308f87c31caf8b9d
Nguyễn Hải Tùng <tungnh1999@Tungs-MacBook-Pro.local>
2021-05-06 23:29:32 +0700

    Update Code May-6

--- src/screen/Messaging/MessagesList/MessagesList.tsx
+++ src/screen/Messaging/MessagesList/MessagesList.tsx
@@ -7,13 +7,25 @@ import {
   List,
   ListItem,
   ListItemText,
+  ListItemAvatar,
   ListItemSecondaryAction,
   Divider,
+  Avatar,
+  Input,
 } from "@material-ui/core";
+
+import FlipMove from "react-flip-move";
 
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
+import Styles from "./Style";
+import { SendRounded } from "@material-ui/icons";
+
 const Messages = () => {
+  const classes = Styles();
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+  const [input, setInput] = React.useState("");
+
   return (
     <div>
       <Paper>
@@ -30,7 +42,7 @@ const Messages = () => {
                     whiteSpace: "nowrap",
                   }}
                 >
-                  <Typography>Hiền Hie</Typography>
+                  <Typography>Long Chu</Typography>
                 </div>
               }
               secondary={
@@ -42,7 +54,7 @@ const Messages = () => {
                     whiteSpace: "nowrap",
                   }}
                 >
-                  <Typography variant="caption">Student at TMU</Typography>
+                  <Typography variant="caption">Student at HUST</Typography>
                 </div>
               }
             />
@@ -55,7 +67,224 @@ const Messages = () => {
         </List>
 
         <Divider />
-        
+
+        <div>
+          <FlipMove
+            style={{
+              height: "400px",
+              overflowY: "scroll",
+            }}
+          >
+            <List component="nav" className={classes.root}>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <Link variant="body2" href="#" color="inherit">
+                      Long Chu
+                    </Link>
+                  }
+                  secondary={
+                    <React.Fragment>
+                      <Typography>
+                        Hello Long chill Hello Long chill Hello Long chill Hello
+                        Long chill Hello Long chill Hello Long chill Hello Long
+                        chill Hello Long chill Hello Long chill Hello Long chill
+                        Hello Long chill Hello Long chill Hello Long chillHello
+                        Long chillHello Long chillvHello Long chillHello Long
+                        chillv
+                      </Typography>
+                    </React.Fragment>
+                  }
+                  style={{ margin: "0" }}
+                />
+              </ListItem>
+            </List>
+
+            <List component="nav" className={classes.root}>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <Link variant="body2" href="#" color="inherit">
+                      Tùng Nguyễn
+                    </Link>
+                  }
+                  secondary={
+                    <React.Fragment>
+                      <Typography>Hello Long chill</Typography>
+                    </React.Fragment>
+                  }
+                />
+              </ListItem>
+            </List>
+
+            <List component="nav" className={classes.root}>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <Link variant="body2" href="#" color="inherit">
+                      Long Chu
+                    </Link>
+                  }
+                  secondary={
+                    <React.Fragment>
+                      <Typography>
+                        Hello Long chill Hello Long chill Hello Long chill Hello
+                        Long chill Hello Long chill Hello Long chill Hello Long
+                        chill Hello Long chill Hello Long chill Hello Long chill
+                        Hello Long chill Hello Long chill Hello Long chillHello
+                        Long chillHello Long chillvHello Long chillHello Long
+                        chillv
+                      </Typography>
+                    </React.Fragment>
+                  }
+                  style={{ margin: "0" }}
+                />
+              </ListItem>
+            </List>
+
+            <List component="nav" className={classes.root}>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <Link variant="body2" href="#" color="inherit">
+                      Long Chu
+                    </Link>
+                  }
+                  secondary={
+                    <React.Fragment>
+                      <Typography>
+                        Hello Long chill Hello Long chill Hello Long chill Hello
+                        Long chill Hello Long chill Hello Long chill Hello Long
+                        chill Hello Long chill Hello Long chill Hello Long chill
+                        Hello Long chill Hello Long chill Hello Long chillHello
+                        Long chillHello Long chillvHello Long chillHello Long
+                        chillv
+                      </Typography>
+                    </React.Fragment>
+                  }
+                  style={{ margin: "0" }}
+                />
+              </ListItem>
+            </List>
+
+            <List component="nav" className={classes.root}>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <Link variant="body2" href="#" color="inherit">
+                      Long Chu
+                    </Link>
+                  }
+                  secondary={
+                    <React.Fragment>
+                      <Typography>
+                        Hello Long chill Hello Long chill Hello Long chill Hello
+                        Long chill Hello Long chill Hello Long chill Hello Long
+                        chill Hello Long chill Hello Long chill Hello Long chill
+                        Hello Long chill Hello Long chill Hello Long chillHello
+                        Long chillHello Long chillvHello Long chillHello Long
+                        chillv
+                      </Typography>
+                    </React.Fragment>
+                  }
+                  style={{ margin: "0" }}
+                />
+              </ListItem>
+            </List>
+
+            <List component="nav" className={classes.root}>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <Link variant="body2" href="#" color="inherit">
+                      Long Chu
+                    </Link>
+                  }
+                  secondary={
+                    <React.Fragment>
+                      <Typography>
+                        Hello Long chill Hello Long chill Hello Long chill Hello
+                        Long chill Hello Long chill Hello Long chill Hello Long
+                        chill Hello Long chill Hello Long chill Hello Long chill
+                        Hello Long chill Hello Long chill Hello Long chillHello
+                        Long chillHello Long chillvHello Long chillHello Long
+                        chillv
+                      </Typography>
+                    </React.Fragment>
+                  }
+                  style={{ margin: "0" }}
+                />
+              </ListItem>
+            </List>
+
+            <List component="nav" className={classes.root}>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary={
+                    <Link variant="body2" href="#" color="inherit">
+                      Long Chu
+                    </Link>
+                  }
+                  secondary={
+                    <React.Fragment>
+                      <Typography>
+                        Hello Long chill Hello Long chill Hello Long chill Hello
+                        Long chill Hello Long chill Hello Long chill Hello Long
+                        chill Hello Long chill Hello Long chill Hello Long chill
+                        Hello Long chill Hello Long chill Hello Long chillHello
+                        Long chillHello Long chillvHello Long chillHello Long
+                        chillv
+                      </Typography>
+                    </React.Fragment>
+                  }
+                  style={{ margin: "0" }}
+                />
+              </ListItem>
+            </List>
+          </FlipMove>
+        </div>
+
+        <Divider />
+
+        <div className={classes.chatFooter}>
+          <form className={classes.form} onSubmit={(e) => console.log(e)}>
+            <Input
+              className={classes.input}
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Type a message..."
+            />
+            <IconButton
+              className={classes.chatButtonIcon}
+              type="submit"
+              disabled={!input}
+              color="primary"
+            >
+              <SendRounded />
+            </IconButton>
+          </form>
+        </div>
       </Paper>
     </div>
   );
--- src/screen/Messaging/MessagesList/Style.ts
+++ src/screen/Messaging/MessagesList/Style.ts
@@ -1,3 +1,35 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export default makeStyles((theme) => ({}));
+export default makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    padding: "0",
+  },
+  chatFooter: {
+    padding: "0.5em 1em",
+    display: "flex",
+    alignItems: "center",
+    borderTop: "1px solid rgba(190, 190, 190, 0.1)",
+  },
+  form: {
+    flex: "1",
+    display: "flex",
+  },
+  input: {
+    flex: 1,
+    padding: "0em 0.8em 0em 1.2em",
+    outline: "none",
+    border: "none",
+    borderRadius: "5px",
+    background: "#868e991a",
+    color: "#000000",
+  },
+  button: { marginLeft: "0.5em" },
+  chatButtonIcon: {
+    color: "rgb(1, 150, 199)",
+    transition: "all 80ms ease-out",
+    // "&:hover": {
+    //   color: "rgb(1, 150, 199)",
+    // },
+  },
+}));
